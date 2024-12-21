684ff861498535f7df984397145d0c65a35008e0
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2024-12-20 22:02:12 -1000

    granite works in Debug only. Run is strange

--- src/t.cpp
+++ src/t.cpp
@@ -21,8 +21,11 @@ static llama_batch batch;
 static void init() {
     sampler_chain_params = llama_sampler_chain_default_params();
     sampler = llama_sampler_chain_init(sampler_chain_params);
-    const float temperature = 0.4;
-    const uint32_t seed = 1234;
+    const float temperature = 0.8f;
+    const uint32_t seed = LLAMA_DEFAULT_SEED;
+    llama_sampler_chain_add(sampler, llama_sampler_init_top_k(50));
+    llama_sampler_chain_add(sampler, llama_sampler_init_top_p(0.9f, 1));
+    llama_sampler_chain_add(sampler, llama_sampler_init_min_p(0.05f, 1));
     llama_sampler_chain_add(sampler, llama_sampler_init_temp(temperature));
     llama_sampler_chain_add(sampler, llama_sampler_init_dist(seed));
     batch = llama_batch_init(512, 0, 1);
@@ -109,14 +112,22 @@ static std::string string_from(const struct llama_context * ctx, const std::vect
     return buf.str();
 }
 
-static std::string detokenize(llama_context * ctx, const std::vector<llama_token> & tokens, bool remove_special = false, bool unparse_special = true) {
+static std::string detokenize(llama_context * ctx, const std::vector<llama_token> & tokens, 
+                              bool remove_special = false, bool unparse_special = true) {
     std::string text;
     text.resize(std::max(text.capacity(), tokens.size()));
-    int32_t n_chars = llama_detokenize(llama_get_model(ctx), tokens.data(), (int32_t)tokens.size(), &text[0], (int32_t)text.size(), remove_special, unparse_special);
+    int32_t n_chars = llama_detokenize(llama_get_model(ctx), 
+                                       tokens.data(), (int32_t)tokens.size(), 
+                                       &text[0], (int32_t)text.size(), 
+                                       remove_special, unparse_special);
     if (n_chars < 0) {
         text.resize(-n_chars);
-        n_chars = llama_detokenize(llama_get_model(ctx), tokens.data(), (int32_t)tokens.size(), &text[0], (int32_t)text.size(), remove_special, unparse_special);
-        GGML_ASSERT(n_chars <= (int32_t)text.size());  // whitespace trimming is performed after per-token detokenization
+        n_chars = llama_detokenize(llama_get_model(ctx), 
+                                   tokens.data(), (int32_t)tokens.size(), 
+                                   &text[0], (int32_t)text.size(), 
+                                   remove_special, unparse_special);
+        // whitespace trimming is performed after per-token detokenization
+        GGML_ASSERT(n_chars <= (int32_t)text.size());  
     }
     text.resize(n_chars);
     // NOTE: the original tokenizer decodes bytes after collecting the pieces.
@@ -144,21 +155,56 @@ static void batch_add(
 }
 
 static bool inference() {
-    // Define the prompt
-    const char* prompt = "Write a short story about a cat.";
-    int32_t len = strlen(prompt);
-    bool add_special = true;
+    llama_chat_message conversation[] = {
+        {"role", "User"},
+        {"content", "Please list one IBM Research laboratory located in the United States. You should only output its name and location."}
+    };
+    std::vector<llama_chat_message> messages;
+    std::vector<char> formatted(llama_n_ctx(ctx));
+    int new_len = llama_chat_apply_template(model, "granite", 
+                                            conversation, 2, 
+                                            true, formatted.data(), formatted.size());
+    if (new_len > (int)formatted.size()) {
+        formatted.resize(new_len);
+        new_len = llama_chat_apply_template(model, "granite", 
+                                            conversation, 2, 
+                                            true, formatted.data(), formatted.size());
+    }
+    if (new_len < 0) {
+        fprintf(stderr, "failed to apply the chat template\n");
+        return false;
+    }
+    formatted.resize(new_len);
+    // this is what actually expected from formatted string:
+    // https://huggingface.co/ibm-granite/granite-3.0-1b-a400m-instruct
+
+    static const char* fs_0 = "<|start_of_role|>user<|end_of_role|>"
+    "Please list one IBM Research laboratory located in the United States. "
+    "You should only output its name and location<|end_of_text|>"
+    "<|start_of_role|>assistant<|end_of_role|>";
+
+    static const char* fs = "<|start_of_role|>user<|end_of_role|>"
+    "Explain battery charge level to voltage ratio for different types of batteries."
+    "<|end_of_text|>"
+    "<|start_of_role|>assistant<|end_of_role|>";
+
+    formatted.resize(strlen(fs));
+    memcpy(formatted.data(), fs, formatted.size());
+    // Python sampel code outputs:
+    // "545 East 9th Street<|end_of_text|>"
+    int32_t len = (int32_t)formatted.size();
+    printf("formatted: \"%s\"\n", formatted.data());
+    bool add_special = false;
     bool parse_special = true;
     int n_tokens = len + 2 * add_special;
     std::vector<llama_token> tokens(n_tokens);
-    n_tokens = llama_tokenize(model, prompt, len, tokens.data(), tokens.size(), 
+    n_tokens = llama_tokenize(model, formatted.data(), len, tokens.data(), tokens.size(), 
                               add_special, parse_special);
     if (n_tokens < 0) {
         fprintf(stderr, "Failed to tokenize the prompt.\n");
         return false;
     }
     tokens.resize(n_tokens);
-    printf("prompt: \"%s\"\n", prompt);
     printf("tokens: %s\n", string_from(ctx, tokens).c_str());
     printf("detokenize: \"%s\"\n", detokenize(ctx, tokens).c_str());
     uint32_t n_ctx = llama_n_ctx(ctx);
@@ -170,7 +216,7 @@ static bool inference() {
         embd.insert(embd.end(), tokens.begin(), tokens.end());
         tokens.clear();
     }
-    int n_predict = 128; // Number of tokens to generate
+    int n_predict = 512; // Number of tokens to generate
     int n_remaining = n_predict;
     while (n_remaining > 0) {
         if (llama_decode(ctx, llama_batch_get_one(embd.data(), embd.size())) != 0) {
@@ -179,22 +225,30 @@ static bool inference() {
         }
         n_past += embd.size();
         llama_token id = llama_sampler_sample(sampler, ctx, -1);
+        // printf("id: %d\n", id);
         if (llama_token_is_eog(model, id)) {
             printf("\n<end of text>\n");
             break;
         }
         std::string token_str = token_to_piece(ctx, id);
-        printf("%zd %d \"%s\"\n", embd.size(), id, token_str.c_str());
+//      printf("%zd %d \"%s\"\n", embd.size(), id, token_str.c_str());
         embd.push_back(id);
-        llama_sampler_accept(sampler, id);
         n_remaining--;
     }
     printf("\n");
+    printf("result: \"%s\"\n", detokenize(ctx, embd).c_str());
     return true;
 }
 
 int main(int argc, char** argv) {
     assert(argc > 1);
+    // only print errors
+    llama_log_set([](enum ggml_log_level level, const char * text, void * /* user_data */) {
+        if (level >= GGML_LOG_LEVEL_ERROR) {
+            fprintf(stderr, "%s", text);
+        }
+    }, nullptr);
+    ggml_backend_load_all();
     ggml_backend_init_best();
     if (!load_model(argv[1])) { return 1; }
     printf("Model loaded and context created.\n");
